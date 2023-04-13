f62351ee58882f279d516791283151e0e2382acc
RadekKisiela <radek1997_97@o2.pl>
2023-04-13 16:31:01 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,10 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ulList = document.querySelector('#ingredients');
+ingredients.forEach((ingredients) => {
+  const liItem = document.createElement('li');
+liItem.textContent = ingredients;
+liItem.classList.add('item');
+ulList.appendChild(liItem);
+});
