f4c120525e515d4d3aab326d2cb224758ea62e64
RadekKisiela <radek1997_97@o2.pl>
2023-04-17 14:54:28 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const button = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+const body = document.body;
+
+function changeBackgroundColor() {
+const randomColor = getRandomHexColor();
+body.style.backgroundColor = randomColor;
+colorSpan.textContent = randomColor;
+}
+
+button.addEventListener('click', changeBackgroundColor);
